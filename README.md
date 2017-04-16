500c3fa98e8ca12cd4cdbab5e8363e1d74511171
Toy <1659808224@qq.com>
2017-04-16 17:45:40 +0800

    add doc

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
-RnDemo
+# react-native 入门简介
+
+## 1. 介绍
+
+* [react 使用文档](https://facebook.github.io/react/docs/hello-world.html)
+
+* [react-native 使用文档](https://facebook.github.io/react-native/docs/getting-started.html)
+
+## 2. 依赖环境
+
+* [nodejs](https://nodejs.org/)
+
+* react-native 命令行工具
+
+        npm install -g react-native-cli
+
+* android
+    * [Android Studio](https://developer.android.com/studio/index.html)  (非必须)
+
+    * [jdk](http://www.oracle.com/technetwork/java/javase/downloads/index.html) >= 1.8
+* ios
+    * [Xcode](https://developer.apple.com/xcode/)
+
+
+
+
+## 3. 如何开始
+
+#### Step1
+
+> 在你的工作目录新建一个项目
+
+    react-native init RnDemo
+
+#### Step2
+
+> 启动react-native
+
+    cd RnDemo
+    npm start
+
+
+#### Step3
+
+> 预览你的应用 （android）
+
+1. 使用android studio 打开 **RnDemo/android** 目录
+2. 配置虚拟机调试或真机调试
+3. 点击启动按钮
+
+#### Step4
+
+> 编辑你的代码
+
+* 入口文件:  
+**RnDemo/index.android.js**  (android)  
+**RnDemo/index.ios.js**  (ios)  
+* 自动刷新：调试菜单中开启live reload
+* 真机调试： 摇一摇手机，打开调试菜单
+* 模拟器调试:
+> 快捷键 (ios)  
+> ⌘ + R  刷新  
+> ⌘ + D  打开调试菜单
+
+
+#### Step5
+
+> 打包发布 (android)
+
+    cd RnDemo/android
+    ./gradlew assembleRelease
+
+然后使用android studio >> build >> Generate Signed APk
+
+配置好密钥
+
+然后build得到 app-release.apk
--- index.ios.js
+++ index.ios.js
@@ -1,35 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class RnDemo extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import React,{ Component } from 'react'
+import {AppRegistry,StyleSheet,Text,View} from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
@@ -48,6 +18,27 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
-});
+})
+
+
+export default class RnDemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+         hello world
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
 
 AppRegistry.registerComponent('RnDemo', () => RnDemo);
